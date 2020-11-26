d5c5af754be1e1ac1c3eb4572f36576c3e811479
naemoo <gusdn0657@gmail.com>
2020-11-26 17:03:52 +0900

    프로그래머스 : 배달

---
+++ Greedy/Dijkstra/Problem01.py
@@ -0,0 +1,41 @@
+#%%
+# https://programmers.co.kr/learn/courses/30/lessons/12978
+import heapq
+from collections import defaultdict
+#%%
+def solution(N, road, K):
+    roads = defaultdict(lambda : defaultdict(lambda : float('inf')))
+
+    for start,end,weight in road:
+        curWeight = min(roads[start][end],weight)
+        roads[start][end] = min(curWeight,weight)
+        roads[end][start] = min(roads[end][start],weight)
+
+    length = [float('inf')] * (N+1)
+    length[1]  = 0 
+    q = []
+    heapq.heappush(q,(0,1))
+
+    while q:
+        w,cur = heapq.heappop(q)
+        if length[cur] < w:
+            continue
+
+        length[cur] = w
+
+        for nextC, nW in roads[cur]:
+            nextWeight = nW + w
+            if length[nextC] > nextWeight:
+                heapq.heappush(q,(nextWeight,nextC))
+
+    answer = 0
+    for k in length:
+        answer += 1 if k <= K else 0
+
+    return answer
+# %%
+solution(5,[[1, 2, 1], [2, 3, 3], [5, 2, 2], [1, 4, 2], [5, 3, 1], [5, 4, 2]],3)
+
+# %%
+roads = defaultdict(lambda : defaultdict(lambda : float('inf')))
+# %%
